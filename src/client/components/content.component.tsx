5913cf5e4f5ad93c2d1ae00cad670eef8165d81d
keysman59 <keysman.dima@yandex.ru>
2018-04-08 10:04:04 +0300

    keysman2

--- src/client/components/content.component.tsx
+++ src/client/components/content.component.tsx
@@ -26,10 +26,14 @@ export class ContentComponent extends React.Component<IContentProps, IContentSta
                 photos: ['https://c.dns-shop.ru/thumb/st1/fit/800/650/d5f2f65ca088547d237d659581b29b20/bc6991119fe73eaaacdb8e142cbb8fc28fbfeb290115386ff1a12b8dde17e534.jpg']
             },
         ];
+
+        let product = <ProductComponent product={products[0]}/>
+
         return (
             <div className="content">
-                Товары
-                {products.map(item => <ProductComponent key={item.id} product={item} />)}
+                <h2>Товары</h2>
+                {product}
+
             </div>
         );
     }
