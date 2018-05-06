32b56d4ed3b52a011d53102453daf45633d00011
Evgeny <it-step@ya.ru>
2018-05-06 09:24:59 +0400

    Basket

--- src/client/reducers/basket.ts
+++ src/client/reducers/basket.ts
@@ -1,15 +1,39 @@
-import { Product } from '../app/classes';
+import { Product, BasketItem } from '../app/classes';
 
 export interface IBasket {
-    items: Product[];
+    items: BasketItem[];
 }
 
-export var basket = function(state: IBasket, action) {
+export var basket = function (state: IBasket, action) {
     if (!state) state = {
         items: [],
     };
     switch (action.type) {
         case 'SET_BASKET': //console.log('SET_BASKET it works');
+        case 'BUY_ITEM':
+            let found = false;
+            let items = state.items.map(item => {
+                if (item.id === action.item.id) {
+                    found = true;
+                    return {
+                        ...item,
+                        count: item.count + 1
+                    }
+                } else
+                    return item;
+            });
+            if (!found)
+                items.push({
+                    id: action.item.id,
+                    count: 1,
+                    name: action.item.name,
+                    price: action.item.price,
+                })
+                
+            return {
+                ...state,
+                items
+            };
         default:
     }
     return state;
